bf8b2e956d58d969d27f63550c19d82e8da0f781
Mauricio Lauffer <443888+mauriciolauffer@users.noreply.github.com>
2023-12-06 09:51:42 +1000

    feat!: Use new sap.ui.core.Lib from v1.120.0

--- src/openui5/validator/library.js
+++ src/openui5/validator/library.js
@@ -5,15 +5,15 @@
  */
 
 sap.ui.define([
-  'sap/ui/core/Core',
+  'sap/ui/core/Lib',
   'sap/ui/core/library'
 ],
 /**
  * Module Dependencies
- * @param {sap.ui.core.Core} Core - sap.ui.core.Core
+ * @param {sap.ui.core.Lib} Lib - sap.ui.core.Lib
  * @returns {object} openui5.validator library
  */
-function(Core) {
+function(Lib) {
   /**
    * OpenUI5 library: openui5.validator.
    * @namespace
@@ -22,7 +22,7 @@ function(Core) {
    * @version ${version}
    * @public
    */
-  Core.initLibrary({
+  return Lib.init({
     name: 'openui5.validator',
     dependencies: [
       'sap.ui.core'
@@ -31,6 +31,4 @@ function(Core) {
     noLibraryCSS: true,
     version: '${version}'
   });
-
-  return openui5.validator; // eslint-disable-line
 });
